061a615260198b1d22cc8d45f2dd34814c813c18
soxface <sonyacorcoran@gmail.com>
2015-04-26 18:58:20 +1000

    added registration page

--- app/app.js
+++ app/app.js
@@ -2,7 +2,8 @@
  
 angular.module('myApp', [
     'ngRoute',
-    'myApp.home'           // Newly added home module
+    'myApp.home',           // Newly added home module
+    'myApp.register'
 ]).
 config(['$routeProvider', function($routeProvider) {
     // Set defualt view of our app to home
