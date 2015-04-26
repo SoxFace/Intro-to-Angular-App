58c4572766f1c7efcf5b1a9270bd76a99d330fe1
soxface <sonyacorcoran@gmail.com>
2015-04-26 18:50:42 +1000

    authenticate on firebase

--- app/home/home.js
+++ app/home/home.js
@@ -1,6 +1,6 @@
 'use strict';
  
-angular.module('myApp.home', ['ngRoute'])
+angular.module('myApp.home', ['ngRoute', 'firebase'])
  
 // Declared route 
 .config(['$routeProvider', function($routeProvider) {
@@ -8,9 +8,30 @@ angular.module('myApp.home', ['ngRoute'])
         templateUrl: 'home/home.html',
         controller: 'HomeCtrl'
     });
+
 }])
  
 // Home controller
-.controller('HomeCtrl', [function() {
- 
+.controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
+
+  var firebaseObj = new Firebase("https://tutsplus-angularapp.firebaseio.com"); 
+  var loginObj = $firebaseSimpleLogin(firebaseObj);
+
+  $scope.user = {};
+    $scope.SignIn = function(e){ 
+       e.preventDefault();
+       var username = $scope.user.email;
+       var password = $scope.user.password;
+       loginObj.$login('password', {
+                  email: username,
+                  password: password
+              })
+              .then(function(user) {
+                  //Success callback
+                  console.log('Authentication successful');
+              }, function(error) {
+                  //Failure callback
+                  console.log('Authentication failure');
+              });
+    }
 }]);
