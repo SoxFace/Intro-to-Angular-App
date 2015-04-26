438812ad6232a332eed9752ec0b4f7f8ca62916d
soxface <sonyacorcoran@gmail.com>
2015-04-26 20:03:25 +1000

    render posts on welcome page

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -1,14 +1,24 @@
 'use strict';
- 
+
 angular.module('myApp.welcome', ['ngRoute'])
- 
+
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/welcome', {
         templateUrl: 'welcome/welcome.html',
         controller: 'WelcomeCtrl'
     });
 }])
- 
-.controller('WelcomeCtrl', ['$scope','CommonProp', function($scope,CommonProp) {
+
+.controller('WelcomeCtrl', ['$scope','$firebase','CommonProp', function($scope,$firebase,CommonProp) {
   $scope.username = CommonProp.getUser();
+  var firebaseObj = new Firebase("https://tutsplus-angularapp.firebaseio.com/Articles");
+      
+         
+  var sync = $firebase(firebaseObj);
+
+  $scope.articles = sync.$asArray();
+  
+  
 }]);
+
+
